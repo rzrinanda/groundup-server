afcfb399d668d1f0672fc2a09389c8168c4dfe28
Rizal Zulfikar Rinanda <rzrinanda@gmail.com>
2022-10-11 10:02:37 +0700

    fix main

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT||3000);
+  await app.listen(process.env.PORT || 3000, function () {
+    // console.log("Express server listening on port %d", this.address().port);
+    console.log("listening on port *:3000");
+  });
 }
 bootstrap();
