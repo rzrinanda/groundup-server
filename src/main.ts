8f679aa3eb6320bc2db43d7f6bc01cb4d3a6bc20
Rizal Zulfikar Rinanda <rzrinanda@gmail.com>
2022-10-12 14:06:36 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors()
   await app.listen(process.env.PORT || 3000, function () {
     // console.log("Express server listening on port %d", this.address().port);
     console.log("listening on port *:3000");
